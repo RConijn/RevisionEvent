d5482802cf69438c2c4b97804d377887ab525cc4
Rianne Conijn <m.a.conijn@tue.nl>
2021-09-09 17:27:58 +0200

    Add files via upload

--- functions/list_zipfiles.R
+++ functions/list_zipfiles.R
@@ -1,8 +1,8 @@
 # Function to list all XML files in zipfolder and unzip them in temporary folder
-list_zipfiles <- function(zipfolder, analysis = "GA"){
+list_zipfiles <- function(zipfolder, analysis){
   
   # list files (depending on the analysis)
-  files <- grep(paste0('output/.*_', analysis, '.*.xml'), 
+  files <- grep(paste0('.*_', analysis, '.*.xml'), 
                 unzip(zipfolder, list=TRUE)$Name, 
                 ignore.case=TRUE, value=TRUE)
   
